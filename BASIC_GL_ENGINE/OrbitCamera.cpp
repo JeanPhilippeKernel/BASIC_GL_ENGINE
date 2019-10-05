c5c37fb17f6d226079ec471214ac6285dbfef2b7
Jean Philippe <jeanphilippe02666592@outlook.fr>
2019-10-05 20:58:52 +0000

    update position of orbit camera

--- BASIC_GL_ENGINE/OrbitCamera.cpp
+++ BASIC_GL_ENGINE/OrbitCamera.cpp
@@ -2,7 +2,7 @@
 
 OrbitCamera::OrbitCamera()
 	:m_radius(0.0f), m_yaw(0.0f), m_pitch(0.0f), 
-	Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f))
+	Camera(glm::vec3(0.0f, 0.0f, 10.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f))
 {
 }
 
@@ -57,7 +57,7 @@ void OrbitCamera::Rotate(float yaw, float pitch)
 	m_yaw = glm::radians(yaw);
 	m_pitch = glm::radians(pitch);
 
-	m_pitch = glm::clamp(m_pitch, -glm::half_pi<float>() + 0.1f, glm::half_pi<float>() - 0.1f);
+	m_pitch = glm::clamp(m_pitch, -glm::half_pi<float>() - 0.1f, glm::half_pi<float>() - 0.1f);
 
 	m_position.x = (m_radius * std::cos(m_pitch) * std::sin(m_yaw)) + m_target.x;
 	m_position.y = (m_radius * std::sin(m_pitch)) + m_target.y;
