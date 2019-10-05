a6cdf00573d78521a1b05333540301c45b2c7065
Jean Philippe <jeanphilippe02666592@outlook.fr>
2019-10-05 20:39:54 +0000

    update app

--- BASIC_GL_ENGINE/Application.cpp
+++ BASIC_GL_ENGINE/Application.cpp
@@ -6,7 +6,19 @@
 #include "Rectangle.h"
 #include "Cube.h"
 #include "Texture2D.h"
+
 #include "FreeCamera.h"
+#include "OrbitCamera.h"
+
+
+OrbitCamera orbitCamera;
+
+float defined_yaw = 0.0f;
+float defined_pitch = 0.0f;
+float defined_radius = 10.0f;
+
+const float MOUSE_SENSITIVITY = 0.25f;
+
 
 inline void MouseInputCallback(GLFWwindow* window, double, double);
 
@@ -105,12 +117,14 @@ int main(int argc, char* argv[])
 
 	//model-view-projection matrix
 	glm::mat4 model_matrix, view_matrix, perspective_matrix;
-														  //
-	model_matrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -1.0f)) * glm::rotate(glm::mat4(1.0f), glm::quarter_pi<float>(), glm::vec3(1.0f, 0.0f, 0.0f));
+														
+	model_matrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -1.0f));
 	
 
-	FreeCamera freeCamera(glm::vec3(0.0f, 0.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)); 
-	view_matrix = freeCamera.GetViewMatrix();
+	
+	orbitCamera.SetRadius(defined_radius);
+	orbitCamera.SetTarget(glm::vec3(0.0f, 0.0f, -1.0f));
+	view_matrix = orbitCamera.GetViewMatrix();
 
 	
 	perspective_matrix = glm::perspective(glm::radians(45.0f), (display.GetWidth() / display.GetHeight()), 0.1f, 100.0f);
@@ -126,7 +140,7 @@ int main(int argc, char* argv[])
 		texture.Bind();
 		texture_two.Bind(1);
 
-		//model_matrix = glm::rotate(glm::mat4(1.0f), glm::radians((float)glfwGetTime() * 50.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		view_matrix = orbitCamera.GetViewMatrix();
 
 		shader.SetUniform("model_matrix", model_matrix);
 		shader.SetUniform("view_matrix", view_matrix);
@@ -146,7 +160,22 @@ int main(int argc, char* argv[])
 
 inline void MouseInputCallback(GLFWwindow* window, double cursor_pos_x, double cursor_pos_y)
 {
-	std::cout << "pos X : " << cursor_pos_x
-		<< " " << "pos Y : " << cursor_pos_y
-		<< std::endl;
+	static glm::vec2 lastMousePos =  glm::vec2(0.0f, 0.0f);
+	
+	if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+	{
+		defined_yaw -= (static_cast<float>(cursor_pos_x) - lastMousePos.x) * MOUSE_SENSITIVITY;
+		defined_pitch += (static_cast<float>(cursor_pos_y) - lastMousePos.y) * MOUSE_SENSITIVITY;
+	}
+
+	if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+	{
+
+	}
+
+	lastMousePos.x = static_cast<float>(cursor_pos_x);
+	lastMousePos.y = static_cast<float>(cursor_pos_y);
+
+
+	orbitCamera.Rotate(defined_yaw,  defined_pitch);
 }
