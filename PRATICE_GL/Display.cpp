c0478a4f73ef2357d15f28a39473b072ff0cafbf
Jean Philippe <jeanphilippe02666592@outlook.fr>
2019-10-28 08:14:55 +0000

    update display

---
+++ PRATICE_GL/Display.cpp
@@ -0,0 +1,26 @@
+#include "Display.h"
+
+
+
+Display::Display()
+{
+}
+
+
+Display::Display(int width, int height, std::string title)
+{
+}
+
+Display::~Display()
+{
+}
+
+bool Display::IsClosed() const
+{
+
+	return false;
+}
+
+void Display::Update()
+{
+}
---
+++ PRATICE_GL/Display.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <string>
+
+					
+class Display
+{
+public:
+	explicit Display(int width, int height, std::string title);
+	~Display();
+
+	bool IsClosed() const;
+	void Update();
+
+
+	int GetHeight() const { return m_height; }
+	int GetWidth() const { return m_width; }
+	const std::string& GetTitle() const { return m_title; }
+	GLFWwindow const * GetCurrentWindow() const { return m_window; }
+
+
+	void SetTitle(const std::string& title) 
+	{
+		if (!title.empty())
+			m_title = title;
+	}
+
+	void SetHeight(int height)
+	{
+		if (height < 600) return;
+		m_height = height;
+	}
+
+	void SetWidth(int width) 
+	{
+		if (width < 800) return;
+		m_width = width;
+	}
+
+
+private:
+	Display();
+	//Display(const Display& display);
+	
+	int m_height, m_width;
+	std::string m_title;
+	double m_elpasedTime;
+
+	GLFWwindow * m_window;
+
+
+	void INITIALIZE_WINDOW_HINT();
+	void INITIALIZE_WINDOW();
+
+	void PROCESSING_FPS();
+};
+
