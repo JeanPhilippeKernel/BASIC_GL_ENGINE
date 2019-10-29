7a0aad0decc1d14ffc7eab1694a992a30363b692
Jean Philippe <jeanphilippe02666592@outlook.fr>
2019-10-29 09:30:52 +0000

    update

--- OBJ_LOADER/Application.cpp
+++ OBJ_LOADER/Application.cpp
@@ -10,32 +10,11 @@
 using namespace std;
 
 
-
-
-
-struct Person
-{
-	string name;
-	int age;
-
-	virtual void Print() {}
-};
-
-
-struct Male : public  Person
-{
-	bool sex{ true };
-	//Male() : Person() {}
-};
-
-struct Female : public Person
-{
-	bool sex{ false };
-	//Female() : Person() {}
-};
-
 int main(int argc, char** argv)
 {
+
+
+
 	system("pause");
 	return 0;
 }
