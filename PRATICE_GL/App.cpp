97ec2267a26d6d1e4faf522d7e098c9114a3802b
Jean Philippe <jeanphilippe02666592@outlook.fr>
2019-10-29 00:52:32 +0000

    update window

--- PRATICE_GL/App.cpp
+++ PRATICE_GL/App.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "Display.h"
+
+
+
+Display display(1000, 800, "GL ENGINE");
 
 
 int main(int argc, char** argv)
 {
+
+	while (!display.IsClosed())
+	{
+		display.Update();
+	}	  
+
 	return 0;
 }
