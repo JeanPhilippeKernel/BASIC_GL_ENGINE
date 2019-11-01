d1e5cb571fd4dd17c48e9bf6063ea2f339f8f8fd
Jean Philippe <jeanphilippe02666592@outlook.fr>
2019-11-01 09:38:16 +0000

    no change

--- PRATICE_GL/App.cpp
+++ PRATICE_GL/App.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include "Display.h"
-#include "Shader.h"
 
 
 
 Display display(1000, 800, "GL ENGINE", true);
-Shader shader("Shaders/basic_vertex.vs", "Shaders/basic_fragment.fs");
 
 
 int main(int argc, char** argv)
@@ -15,6 +13,9 @@ int main(int argc, char** argv)
 	{
 		display.ClearColor(.2f, .5f, .3f, 1.0f);
 		display.EnableDepthTesting();
+
+
+
 
 		display.Update();
 	}	  
